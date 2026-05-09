0195f8018cb4f50c5ea2ebd7da70a395458ac16f
louiss0 <louisshelton0@gmail.com>
2026-05-08 20:15:28 -0400

    test(cmd): add import conversion test scaffold

---
+++ cmd/import_convert.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func importYAMLSource(path string, input string) (string, error) {
+	return "", fmt.Errorf("import yaml: not implemented")
+}
+
+func importTOMLSource(path string, input string) (string, error) {
+	return "", fmt.Errorf("import toml: not implemented")
+}
